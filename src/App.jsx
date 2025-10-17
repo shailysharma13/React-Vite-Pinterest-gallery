c238abc09fd3776dc779cd4c7181cece0cb5676d
Shaily Sharma <shailysharma13@gmail.com>
2025-10-17 15:27:42 +0530

    feat: Added env, Gallery,useSplash to FetchAPI and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Gallery from "./comonents/Gallery";
+
 export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-gray-800">
-        React Unsplash Gallery
+        <Gallery/>
       </h1>
     </div>
   );
